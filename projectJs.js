e43ec7a0b087c9b3d3a4e0b57098fb18baa038eb
simplemilk <123520221+simplemilk@users.noreply.github.com>
2023-07-17 21:31:28 -0400

    Add files via upload
    Final changes to the project

--- projectJs.js
+++ projectJs.js
@@ -1,10 +1,96 @@
-document.getElementById("Submit").addEventListener('click',myWindow)
-function myWindow()
-{
-    visitor = document.getElementById("Name").value;
-myText = ("<html>\n<head>\n<title>Welcome</title>\n</head>\n<body>\n");
-    myText += ("Hello " + visitor);
+function generateMealPlan() {
 
-flyWindow = window.open('about:blank','myPop','width=400,height=200,left=200,top=200');
-flyWindow.document.write(myText);
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var goal = document.getElementById('goal').value;
+
+
+    if (!validateEmail(email)) {
+        alert('Please enter a valid email address.');
+        return;
+    }
+
+
+    var mealPlan = [];
+    for (var i = 0; i < 7; i++) {
+        var breakfast = document.getElementById('breakfast' + (i + 1)).value;
+        var snack = document.getElementById('snack' + (i + 1)).value;
+        var lunch = document.getElementById('lunch' + (i + 1)).value;
+        var dinner = document.getElementById('dinner' + (i + 1)).value;
+
+        mealPlan.push({
+            day: getDayOfWeek(i),
+            breakfast: breakfast,
+            snack: snack,
+            lunch: lunch,
+            dinner: dinner
+        });
+    }
+
+
+    var mealPlanPage = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <title>Meal Plan</title>
+        <style>
+        body {}
+        </style>
+        </head>
+        <body>
+        `;
+
+    mealPlanPage += `<h1>Weekly Meal Plan for ${name}</h1>
+        <h2>Goal for the Week:</h2>
+        <p>${goal}</p>
+        `;
+
+
+    for (var j = 0; j < mealPlan.length; j++) {
+        var dayPlan = mealPlan[j];
+        mealPlanPage += `<h2>${dayPlan.day}</h2>
+        <p> Breakfast:
+            ${dayPlan.breakfast}</p>
+        <p> Snack:
+            ${dayPlan.snack}</p>
+        <p> Lunch:
+            ${dayPlan.lunch}</p>
+        <p> Dinner and Snack:
+            ${dayPlan.dinner}</p>
+        `;
+    }
+
+    mealPlanPage +=
+        '</body>\n</html>';
+
+    var mealPlanWindow = window.open('', '_blank');
+    mealPlanWindow.document.open();
+    mealPlanWindow.document.write(mealPlanPage);
+    mealPlanWindow.document.close();
+}
+
+function clearMealPlan() {
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('goal').value = '';
+
+    for (var i = 1; i <= 7; i++) {
+        document.getElementById('breakfast' + i).value = '';
+        document.getElementById('snack' + i).value = '';
+        document.getElementById('lunch' + i).value = '';
+        document.getElementById('dinner' + i).value = '';
+
+    }
+}
+
+function validateEmail(email) {
+
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function getDayOfWeek(index) {
+
+    var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    return days[index];
 }
